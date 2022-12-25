e4a7ef837fd9549d4a1e46fbcf054229cddf7ee7
Ksenia <mineeva.94@inbox.ru>
2022-12-25 12:59:16 +0300

    Домашнее задание 6

---
+++ task43/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 43 Напишите программу, которая найдёт
+точку пересечения двух прямых, заданных
+уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.*/
+
+Console.Clear();
+Console.Write("Введение через пробел b1, k1, b2, k2: ");
+string[] f = Console.ReadLine().Split();
+double b1 = double.Parse(f[0]);
+double k1 = double.Parse(f[1]);
+double b2 = double.Parse(f[2]);
+double k2 = double.Parse(f[3]);
+
+Console.WriteLine(String.Join(" ", GetPoint(b1, k1, b2, k2)));
+
+double[] GetPoint(double inB1, double inK1, double inB2, double inK2)
+
+{
+    double[] result = new double[2];
+    result[0] = (inB2 - inB1) / (inK1 - inK2);
+    result[1] = inK1 * result[0] + inB1;
+    return result;
+}
